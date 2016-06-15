cbca624f9c05063b31c60abd7784523a535e6950
scott holland <scotthollandjr@gmail.com>
2016-06-15 12:51:46 -0700

    Fix api connection with updated appending html

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,10 +1,9 @@
 var searchResults = require('./../js/search.js').searchResults;
 
 $(document).ready(function() {
-  $('#searchForm').submit(function(event) {
+  $('#resultsButton').click(function(event) {
     event.preventDefault();
     var username = $('#username').val();
-    $('#printUser').append(username);
     searchResults(username);
   })
 })
--- js/search.js
+++ js/search.js
@@ -1,10 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.searchResults = function(username) {
-  $.get('http://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
-    response.forEach(function(entry) {
+  $.get('http://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response) {
+    console.log(response);
+    var responses = response;
+    responses.forEach(function(entry) {
       console.log(entry);
-      $('#printResults').append('<li><p><a href="' + entry.url + '">' + entry.name + '</a><p>' + '</p><p>' + entry.description + '</p></li>');
+      $('#printResults').append('<li><p><a href="' + entry.html_url + '">' + entry.name + '</a><p>' + '</p><p>'+ entry.description + '<p>Created on: ' + entry.created_at + '</p></li>');
     });
   }).fail(function(error) {
     console.log(error.responseJSON.message);
